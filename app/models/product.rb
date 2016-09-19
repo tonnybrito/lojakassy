c855c3e64712b74ccddaa0a9fbd004ae7789e785
Estágio em Programação <curso@estagioemprogramacao.com>
2016-09-19 16:26:52 -0300

    Validações adicionadas ao model Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :category, presence: true, length: 1..20
+  validates :title, presence: true, length: 3..100
+  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :image, presence: true,
+    format: {with:/\A(http(s|)?:\/\/.*\.(?:png|jpg))\Z/i}
+  validates :description, length: { maximum: 1500 }
 end
