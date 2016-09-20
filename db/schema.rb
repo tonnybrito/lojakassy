d7a86728f3c1829fa89e65ea6a60a628b850af23
Estágio em Programação <curso@estagioemprogramacao.com>
2016-09-20 10:49:19 -0300

    Criação de model para contabilizar visualizações
    Comandos:
    - rails generate model View remote_ip:string
    - rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919175257) do
+ActiveRecord::Schema.define(version: 20160920134231) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "category"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160919175257) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+  end
+
+  create_table "views", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "remote_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
