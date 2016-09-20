0fbc741b67610ba945aaa36f9060e68fb79cb590
Estágio em Programação <curso@estagioemprogramacao.com>
2016-09-19 21:46:52 -0300

    Criação da página de produto

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,8 @@ class StoreController < ApplicationController
   def index
     @products = Product.all.shuffle
   end
+
+  def product
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products
+
+  get 'produtos/:id', to: 'store#product'
   # get 'store/index'
+
   root 'store#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
