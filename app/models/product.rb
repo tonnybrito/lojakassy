76a10b905bc0b3e1ffbb9f3c43ee222e9ce63447
tonnybrito <tonny@devlandia.net>
2022-09-23 20:45:03 -0300

    lojakassy

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,23 @@ class Product < ApplicationRecord
   validates :category, presence: true, length: 1..20
   validates :title, presence: true, length: 3..100
   validates :price, presence: true, numericality: {greater_than: 0}
-  validates :image, presence: true,
-    format: {with:/\A(http(s|)?:\/\/.*\.(?:png|jpg))\Z/i}
+  validates :image, presence: true
   validates :description, length: { maximum: 1500 }
+
+  def image=(value)
+    if value.is_a?(String)
+      super(value)
+    else
+      image = "#{Time.now.to_i}-#{value.original_filename}"
+      caminho_da_imagem = "#{Rails.root}/public/images/"
+      caminho_com_imagem = "#{caminho_da_imagem}#{image}"
+
+      Dir.mkdir(caminho_da_imagem) unless Dir.exist?(caminho_da_imagem) 
+      File.open(caminho_com_imagem, "wb") { |f| f.write(value.read)}
+
+      super("/images/#{image}")
+    end
+  end
 
   def self.categories
     records = Product.select(:category).group(:category).all
